1f15a4d299e662f240154e74cab257f585b2ac89
antoniocapelo <antonio.c.capelo@gmail.com>
2023-07-26 11:17:29 +0100

    analytics

--- src/initiGA.ts
+++ src/initiGA.ts
@@ -3,7 +3,9 @@ import ReactGA from "react-ga";
 export const initGA = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const id = import.meta.env.VITE_GA;
-  if (process.env.NODE_ENV === "production") {
-    ReactGA.initialize(id as string);
-  }
+  // if (process.env.NODE_ENV === "production") {
+  console.log("starting GA…");
+
+  ReactGA.initialize(id as string);
+  // }
 };
