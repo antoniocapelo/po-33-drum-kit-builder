db5e8fa37547fb2dac9fa2a765a9206c8d01e6cd
antoniocapelo <antonio.c.capelo@gmail.com>
2023-07-26 11:31:39 +0100

    analytics

--- src/initiGA.ts
+++ src/initiGA.ts
@@ -1,12 +1,22 @@
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export const initGA = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const id = import.meta.env.VITE_GA;
-  if (process.env.NODE_ENV === "production") {
+  const id = import.meta.env.VITE_GA as string;
+  if (id && process.env.NODE_ENV === "production") {
     console.log("starting GA…");
 
-    ReactGA.initialize(id as string, { debug: true });
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.initialize([
+      {
+        trackingId: id,
+      },
+    ]);
+
+    // Send pageview with a custom path
+    ReactGA.send({
+      hitType: "pageview",
+      page: `${window.location.pathname}${window.location.search}`,
+      title: "PO-33 util",
+    });
   }
 };
